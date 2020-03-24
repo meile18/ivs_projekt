bc385a0da5b504f5b39d7d0d6c51c6fb43fbd0ad
Viliam <podhajeckyv@gmail.com>
2020-03-24 17:04:19 +0100

    Update GUI v1.1
    import GUI from .ui file

--- src/gui.py
+++ src/gui.py
@@ -1,10 +1,24 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-import PyQt5.QtWidgets
+from PyQt5.QtWidgets import QPushButton, QToolButton, QLabel
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtCore import pyqtSlot
 
-app = QtWidgets.QApplication([])
-win = uic.loadUi("gui.ui")
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()  # Call the inherited classes __init__ method
+        uic.loadUi('gui.ui', self)  # Load the .ui file
 
-win.show()
+# testovanie vypisu po kliknuti sum
+        self.button = self.findChild(QtWidgets.QPushButton, 'sum')
+        self.button.clicked.connect(self.printButtonSum)
+####
+        self.show()                 # Show the GUI
 
-sys.exit(app.exec())
+# testovanie vypisu
+    def printButtonSum(self):
+        print("ahoj")
+####
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
